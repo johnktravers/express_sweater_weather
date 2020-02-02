bc51398f0a85472b89825241ff7bde40afb7b034
John Travers <johnk.travers@gmail.com>
2020-02-02 14:22:20 -0700

    Add sad path tests for api key and location parameters

--- tests/addFavorites.spec.js
+++ tests/addFavorites.spec.js
@@ -28,4 +28,50 @@ describe('New favorites api endpoint', () => {
     expect(res.statusCode).toBe(200);
     expect(res.body.message).toBe('Seville, Spain has been added to your favorites');
   });
+
+  test('It sees an error message if api key is missing', async () => {
+    const res = await request(app)
+      .post('/api/v1/favorites')
+      .send({location: 'sevilla, spain'})
+      .type('form');
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.status).toBe(401);
+    expect(res.body.message).toBe('Missing or invalid API key. Please try again.')
+  });
+
+  test('It sees an error message if api key is invalid', async () => {
+    const res = await request(app)
+      .post('/api/v1/favorites')
+      .send({api_key: 'sg2o3u4thrkjng9842', location: 'sevilla, spain'})
+      .type('form');
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.status).toBe(401);
+    expect(res.body.message).toBe('Missing or invalid API key. Please try again.')
+  });
+
+  test('It sees an error if the location is missing', async () => {
+    const res = await request(app)
+      .post('/api/v1/favorites')
+      .send({api_key: '12345'})
+      .type('form');
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.status).toBe(400);
+      expect(res.body.message).toBe('Location not found. Please try again.')
+
+  });
+
+  test('It sees an error if the location is invalid', async () => {
+    const res = await request(app)
+      .post('/api/v1/favorites')
+      .send({api_key: '12345', location: 'sdkjglhaitgjkb'})
+      .type('form');
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.status).toBe(400);
+      expect(res.body.message).toBe('Location not found. Please try again.')
+
+  });
 });
