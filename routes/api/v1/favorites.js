f94a811ac24232608470be4a8f3d57a8bbf37e53
John Travers <johnk.travers@gmail.com>
2020-02-02 20:16:46 -0700

    Add get favorites endpoint to list favorite locations with weather

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -9,6 +9,25 @@ const fetch = require('node-fetch');
 const { URL, URLSearchParams } = require('url');
 
 const Geocode = require('../../../lib/objects/geocode');
+const Forecast = require('../../../lib/objects/forecast');
+const CurrentForecast = require('../../../lib/objects/currentForecast');
+
+
+router.get('/', async function(req, res, next) {
+  let apiKey = req.body.api_key || '';
+
+  try {
+    let user = await validateUser(apiKey)
+    let locations = await database.from('locations')
+      .innerJoin('user_locations', 'user_locations.location_id', 'locations.id')
+      .where('user_locations.user_id', user.id)
+
+    let forecasts = await getFavoritesForecasts(locations);
+    res.status(200).json({ favorite_locations: forecasts })
+  } catch {
+    res.status(404).json({ error: '404 Not Found' })
+  }
+});
 
 router.post('/', async function(req, res, next) {
   let location = req.body.location || '';
@@ -79,5 +98,32 @@ function getGeocodeData(location) {
 
   return geocode_data;
 };
+
+// Dark Sky API Call
+
+function getForecast(geocode) {
+  let apiKey = process.env.DARK_SKY_API_KEY;
+  let url = new URL(`https://api.darksky.net/forecast/${apiKey}/${geocode.lat},${geocode.long}`);
+
+  let forecast = fetch(url)
+    .then(response => response.json())
+    .then(json => createCurrentForecast(geocode, json))
+    .catch(error => console.log(error));
+
+  return forecast;
+};
+
+function createCurrentForecast(geocode, forecast_data) {
+  return {
+    location: geocode.address,
+    current_weather: new CurrentForecast(forecast_data.currently)
+  };
+}
+
+async function getFavoritesForecasts(locations) {
+  return Promise.all(
+    locations.map(location => getForecast(location))
+  );
+}
 
 module.exports = router;
